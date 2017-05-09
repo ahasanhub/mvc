9f94987de02f299e395813ae617a2aa12b125d21
Ahasan <shobuz.cse03@gmail.com>
2017-05-09 15:18:42 +0600

    update from office

---
+++ FluentValidationSample/FluentValidationSample/Models/BootstrapModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FluentValidationSample.Models
+{
+    public class BootstrapModel
+    {
+        public int ID { get; set; }
+        public string AreaLabeledId { get; set; }
+    }
+}
